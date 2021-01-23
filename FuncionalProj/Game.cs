a89f423d1e44072fa8a5de2a94751e888c002c07
Gabriel Alves <gabrielalves.ifpb@gmail.com>
2021-01-22 23:23:09 -0300

    Estrutura base para jogos a serem cadastrados // Struct for the inserted games

---
+++ FuncionalProj/Game.cs
@@ -0,0 +1,13 @@
+namespace FuncionalProj
+{
+    public struct Game
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public string Organization { get; set; }
+        public string Evaluation { get; set; }
+        public double Grade { get; set; }
+        
+    }
+}
